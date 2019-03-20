f9922e2f215dbe4dee69eaf006c56f66522e213e
chen.zhang <chen.zhang@chenzhangs-MacBook-Pro.local>
2019-03-20 20:08:11 +0800

    layout

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,38 @@
-import React from "react";
+import React,{useState} from "react";
 import ReactDOM from "react-dom";
-import { Router } from "react-router-dom";
-import {Main} from './pages/layout/main';
+import { Router,Switch,Route,Link } from "react-router-dom";
 import {history} from '@src/common/history';
+import {Counter} from './pages/layout/counter';
 import './style/index.scss';
+
+function Home() {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+      <br />
+      <Link to="/counter">To Counter</Link>
+    </div>
+  );
+}
+
+function Login() {
+  window.setTimeout(()=>history.push('/'),2000)
+  return (
+    <div>login....</div>
+  )
+}
 
 ReactDOM.render(
   <Router history={history}>
-    <Main />
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/login" component={Login} />
+      <Route path="/counter" component={Counter}/>
+    </Switch>
   </Router>,
   document.getElementById("app")
 );
